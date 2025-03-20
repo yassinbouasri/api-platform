9d4c9f69fbf9dd5650ead9cdd0e1408cdefc4884
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-03-20 16:44:37 +0100

    - Hello Operations - Customizing Operations - ApiResource Options - Changing the shortName

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -3,12 +3,28 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\DragonTreasureRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    shortName: 'Treasure',
+    description: 'A rare and valuable treasure',
+    operations: [
+        new Get(uriTemplate: '/dragon-plunder/{id}'),
+        new GetCollection(uriTemplate: '/dragon-plunder'),
+        new Post(),
+        new Put(),
+        new Patch(),
+    ]
+)]
 class DragonTreasure
 {
     #[ORM\Id]
